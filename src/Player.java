c8e5663043c9b79a9d3f1ad4bb7f723027408c0f
Maykon deepin <maykonmichelpalma@gmail.com>
2019-06-16 17:59:18 -0300

    Added Player class

---
+++ src/Player.java
@@ -0,0 +1,13 @@
+class Player {
+    private int id;
+    private int score;
+
+    Player(int id) {
+        this.id = id;
+        score = 0;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
